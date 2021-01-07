7797a4b1a7aa0fdc300989cb942c677e88aa3e04
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-01-07 16:23:37 +0800

    [EASY_0009_PALINDROME_NUMBER] complete

---
+++ EASY_0009_PALINDROME_NUMBER/src/main/java/com/peter12/solution/Solution.java
@@ -0,0 +1,63 @@
+package com.peter12.solution;
+
+import java.util.List;
+import java.util.Vector;
+
+public class Solution {
+	
+	public static boolean isPalindrome(int x) {
+		if( x < 0 ) {
+			 return false;
+		}
+		
+        int reverse = reverse(x);
+        
+        if( reverse == x ) {
+        	 return true;
+        } else {
+        	return false;
+        }
+        
+    }
+	
+	public static int reverse(int x) {
+
+		int data = x;
+		List<Integer> digit = new Vector<Integer>();
+		boolean negative = data < 0;
+
+		if( negative) {
+			data = -data;
+		}
+
+		while( data != 0 ) {
+			digit.add(data % 10);
+
+			data /= 10;
+		}
+
+
+		double result = 0;
+		int size = digit.size() - 1;
+		for( int i = 0; i <= size ; i++) {
+			result += Math.pow(10, size - i) * digit.get(i);
+
+		}
+
+		if(negative) {
+			result = -result;
+		}
+
+
+		double max = Math.pow(2, 31) - 1;
+		double min = -Math.pow(2, 31);
+
+	    if( min <= result && result <= max ) {
+	    	return (int) result;
+	    } else {
+	    	return 0;
+	    }
+			
+		
+	}
+}
