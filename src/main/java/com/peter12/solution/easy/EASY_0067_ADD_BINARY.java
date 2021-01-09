823dc6675750b8d17416d4d621b8e2d2215e1d5e
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-01-09 16:24:06 +0800

    [EASY_0067_ADD_BINARY] complete

---
+++ src/main/java/com/peter12/solution/easy/EASY_0067_ADD_BINARY.java
@@ -0,0 +1,73 @@
+package com.peter12.solution.easy;
+
+public class EASY_0067_ADD_BINARY {
+	public static String addBinary(String a, String b) {
+		
+		//Alignment
+		String addend = a;
+		String summand = b;
+		int missingDigit = a.length() - b.length();
+		if( missingDigit > 0 ) {
+			summand = addLeading0Digit(summand, missingDigit);
+		} 
+		if(missingDigit < 0){
+			addend = addLeading0Digit(addend, -missingDigit);
+		}
+		
+
+		String result = "";
+		
+		int[] caddend = changeToIntArray(addend);
+		int[] csummand = changeToIntArray(summand);
+		
+		for(int i = caddend.length - 1; i >= 0; i--) {
+			int val = caddend[i] + csummand[i];
+			
+			if( val < 2 ) {
+				result = val + result;
+			} else {
+				result = "0" + result;
+				//Carry
+				if( i - 1 < 0 ) {
+					result = "1" + result;
+				} else {
+					caddend[i-1]++;
+				}
+				
+				
+			}
+		}
+		
+		return result;
+	}
+	
+	public static int[] changeToIntArray(String a ) {
+		int[] result = new int[a.length()];
+		
+		char[] data = a.toCharArray();
+		for( int i = 0; i < data.length; i++ ) {
+			if( data[i] == '0' ) {
+				result[i] = 0;
+			} 
+			
+			if( data[i] == '1' ) {
+				result[i] = 1;
+			}
+		}
+		
+		return result;
+	}
+	
+	public static String addLeading0Digit(String data, int leadingSize) {
+		StringBuilder result = new StringBuilder();
+		for( int i = 0; i < leadingSize; i++ ) {
+			result.append("0");
+		}
+		
+		result.append(data);
+		
+		return result.toString();
+		
+		
+	}
+}
---
+++ src/test/java/com/peter12/solution/easy/EASY_0067_ADD_BINARY_TEST.java
@@ -0,0 +1,62 @@
+package com.peter12.solution.easy;
+
+import java.util.List;
+import java.util.Vector;
+
+import com.peter12.solution.data.ListNode;
+import com.peter12.solution.easy.EASY_0020_VALID_PARENTHESES;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class EASY_0067_ADD_BINARY_TEST 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EASY_0067_ADD_BINARY_TEST( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( EASY_0067_ADD_BINARY_TEST.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    
+    public static void testGetResult() {
+    	Assert.assertEquals("10101", EASY_0067_ADD_BINARY.addBinary("1010", "1011"));
+    	
+    	Assert.assertEquals("10", EASY_0067_ADD_BINARY.addBinary("1", "1"));
+    	Assert.assertEquals("0", EASY_0067_ADD_BINARY.addBinary("0", "0"));
+    	Assert.assertEquals("1", EASY_0067_ADD_BINARY.addBinary("0", "1"));
+    	Assert.assertEquals("1", EASY_0067_ADD_BINARY.addBinary("1", "0"));
+    	
+    	
+    	Assert.assertEquals("100", EASY_0067_ADD_BINARY.addBinary("11", "1"));
+    	
+    
+    }
+    
+   
+}
