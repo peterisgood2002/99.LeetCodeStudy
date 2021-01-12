8e88bf7793639e41e205333e0fb729ac4770c594
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-01-12 16:03:21 +0800

    [EASY_0119_PASCALS_TRIANGLE_II] complete

---
+++ src/main/java/com/peter12/solution/easy/EASY_0119_PASCALS_TRIANGLE_II.java
@@ -0,0 +1,36 @@
+package com.peter12.solution.easy;
+
+import java.util.List;
+import java.util.Vector;
+
+public class EASY_0119_PASCALS_TRIANGLE_II {
+	public static List<Integer> getRow(int rowIndex) {
+
+		List<Integer> result = new Vector<Integer>();
+		
+		for( int i = 0; i < rowIndex + 1; i++ ) {
+			int c = (int)C(rowIndex, i);
+			
+			result.add(c);
+		}
+		return result;
+	}
+
+	public static double C(int m, int n ) {
+		if( n == 0 ) {
+			return 1;
+		}
+		
+		double numerator = 1;
+		for( int i = m; i > m - n; i-- ) {
+			numerator *= i;
+		}
+
+		double denominator = 1;
+		for( int i = 1; i <= n; i++ ) {
+			denominator *= i;
+		}
+
+		return numerator / denominator;
+	}
+}
---
+++ src/test/java/com/peter12/solution/easy/EASY_0119_PASCALS_TRIANGLE_II_TEST.java
@@ -0,0 +1,88 @@
+package com.peter12.solution.easy;
+
+import java.util.List;
+import java.util.Vector;
+
+import com.peter12.solution.data.ListNode;
+import com.peter12.solution.data.TreeNode;
+import com.peter12.solution.data.TreeNodeData;
+import com.peter12.solution.easy.EASY_0020_VALID_PARENTHESES;
+import com.peter12.solution.util.Util;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class EASY_0119_PASCALS_TRIANGLE_II_TEST 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EASY_0119_PASCALS_TRIANGLE_II_TEST( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( EASY_0119_PASCALS_TRIANGLE_II_TEST.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    
+    public static void testGetResult() {
+    	
+    	
+    	List<Integer> r1 = new Vector<Integer>();
+    	r1.add(1);
+    	Assert.assertTrue(Util.equalForList(r1, EASY_0119_PASCALS_TRIANGLE_II.getRow(0)));
+    	
+    	r1 = new Vector<Integer>();
+    	r1.add(1);
+    	r1.add(1);
+    	Assert.assertTrue(Util.equalForList(r1, EASY_0119_PASCALS_TRIANGLE_II.getRow(01)));
+    	
+    	r1 = new Vector<Integer>();
+    	r1.add(1);
+    	r1.add(2);
+    	r1.add(1);
+    	Assert.assertTrue(Util.equalForList(r1, EASY_0119_PASCALS_TRIANGLE_II.getRow(2)));
+    	
+    	r1 = new Vector<Integer>();
+    	r1.add(1);
+    	r1.add(3);
+    	r1.add(3);
+    	r1.add(1);
+    	Assert.assertTrue(Util.equalForList(r1, EASY_0119_PASCALS_TRIANGLE_II.getRow(3)));
+    	
+    	r1 = new Vector<Integer>();
+    	r1.add(1);
+    	r1.add(4);
+    	r1.add(6);
+    	r1.add(4);
+    	r1.add(1);
+    	Assert.assertTrue(Util.equalForList(r1, EASY_0119_PASCALS_TRIANGLE_II.getRow(4)));
+    	
+    	
+    	
+    }
+    
+   
+}
