b8fc39431a392eae48f2f10b44400c0f5cee78f5
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-01-26 17:19:54 +0800

    [EASY_0453_MINIMUM_MOVES_TO_EQUAL_ARRAY_ELEMENTS] Solution

--- src/main/java/com/peter12/solution/easy/EASY_0453_MINIMUM_MOVES_TO_EQUAL_ARRAY_ELEMENTS.java
+++ src/main/java/com/peter12/solution/easy/EASY_0453_MINIMUM_MOVES_TO_EQUAL_ARRAY_ELEMENTS.java
@@ -5,19 +5,23 @@ import java.util.Arrays;
 public class EASY_0453_MINIMUM_MOVES_TO_EQUAL_ARRAY_ELEMENTS {
 	public static int minMoves(int[] nums) {
 	
+		/**
+		 * 
+		 * Solution: https://medium.com/@terracotta_ko/leetcode-453-minimum-moves-to-equal-array-elements-b1a8da3ff6d8
+		 */
 		/*
-		 *   2  6  9
-		 *   3  7  9
-		 *   4  8  9
-		 *   5  9  9 ==> move 3 = (9 - 6) * 1
-		 *   6 10  9
-		 *   7 10 10
-		 *   8 11 10
-		 *   9 11 11
-		 *  10 12 11
-		 *  11 12 12
-		 *  12 13 12
-		 *  13 13 13 ==> move 8 = (6 - 2) * 2
+		 *             2  6  9
+		 * (2 6 8)+1   3  7  9
+		 * (2 6 7)+2   4  8  9
+		 * (2 6 6)+3   5  9  9 ==> move 3 = (9 - 6) * 1
+		 * (2 6 5)+4   6 10  9
+		 * (2 5 5)+5   7 10 10
+		 * (2 5 5)+6   8 11 10
+		 * (2 4 4)+7   9 11 11
+		 * (2 4 3)+8  10 12 11
+		 * (2 3 3)+9  11 12 12
+		 * (2 3 2)+10 12 13 12
+		 * (2 2 2)+11 13 13 13 ==> move 8 = (6 - 2) * 2
 		 *  
 		 *    1  2  3  4  5
 		 *    2  3  4  5  5 ==> move 1 = (5 - 4 ) * 1
