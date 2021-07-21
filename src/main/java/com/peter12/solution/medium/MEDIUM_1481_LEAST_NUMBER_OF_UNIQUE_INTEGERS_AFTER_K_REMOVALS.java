7d0e47b932a7188b33b153ce42b9a8bfa8b180aa
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-07-21 17:15:06 +0800

    [MEDIUM_1481_LEAST_NUMBER_OF_UNIQUE_INTEGERS_AFTER_K_REMOVALS] Simple Solution

--- src/main/java/com/peter12/solution/medium/MEDIUM_1481_LEAST_NUMBER_OF_UNIQUE_INTEGERS_AFTER_K_REMOVALS.java
+++ src/main/java/com/peter12/solution/medium/MEDIUM_1481_LEAST_NUMBER_OF_UNIQUE_INTEGERS_AFTER_K_REMOVALS.java
@@ -1,12 +1,16 @@
 package com.peter12.solution.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class MEDIUM_1481_LEAST_NUMBER_OF_UNIQUE_INTEGERS_AFTER_K_REMOVALS {
 	public static int findLeastNumOfUniqueInts(int[] arr, int k) {
+		/*
+		 * Solution: https://www.cnblogs.com/cnoodle/p/14524251.html
+		 * */
         HashMap<Integer, Integer> count = new HashMap<Integer, Integer>();
         for( int val : arr ) {
             Integer c = count.get(val);
@@ -19,44 +23,26 @@ public class MEDIUM_1481_LEAST_NUMBER_OF_UNIQUE_INTEGERS_AFTER_K_REMOVALS {
             count.put(val, c);
         }
         
-        HashMap<Integer, List<Integer>> cList = new HashMap<Integer, List<Integer>>();
-        List<Integer> key = new ArrayList<Integer>();
-        for( HashMap.Entry<Integer, Integer> e : count.entrySet() ) {
-            List<Integer> l = cList.get( e.getValue() );
+        int size = count.size();
+        int[] c = new int[size];
+        int i = 0;
+        for( int key : count.keySet() ) {
+            c[i] = count.get(key);
             
-            if( l == null ) {
-                l = new ArrayList<Integer>();
-                cList.put(e.getValue(), l );
-                
-                key.add( e.getValue() );
-            }
-            
-            l.add(e.getKey() );
+            i++;
         }
         
-        Collections.sort(key);
-        
-        int result = 0;
-        for( int c : key ) {
-            List<Integer> l = cList.get(c);
-            
-            if( k <= 0 ) {
-                result += l.size();
+        Arrays.sort(c);
+        for( int v : c ) {
+            if( k >= v ) {
+                k-= v;
+                size--;
             } else {
-                 int remove = k / c;
-            
-                if( remove > l.size() ) {
-                     k -= c * l.size();
-                } else {
-                    result += l.size() - remove;
-                    k -= c * remove;
-                }  
+                break;
             }
-            
         }
         
-        
-        return result;
+        return size;
         
         
     }
