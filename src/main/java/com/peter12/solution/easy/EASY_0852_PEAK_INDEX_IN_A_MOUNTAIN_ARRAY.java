79024ef113ed4319ec7b6bbe7d4d6b38b39d3354
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-07-27 08:45:38 +0800

    [EASY_0852_PEAK_INDEX_IN_A_MOUNTAIN_ARRAY] Solution

--- src/main/java/com/peter12/solution/easy/EASY_0852_PEAK_INDEX_IN_A_MOUNTAIN_ARRAY.java
+++ src/main/java/com/peter12/solution/easy/EASY_0852_PEAK_INDEX_IN_A_MOUNTAIN_ARRAY.java
@@ -4,36 +4,19 @@ public class EASY_0852_PEAK_INDEX_IN_A_MOUNTAIN_ARRAY {
 	public int peakIndexInMountainArray(int[] arr) {
 
 		int begin = 0;
-		int end = arr.length - 1;
-
-		while( begin <= end ) {
-
-			int middle = ( begin + end ) / 2;
-
-			if( begin + 1 == end ) {
-				if( arr[begin] > arr[end ] ) {
-					return begin;
-				} else {
-					return end;
-				}
-			}
-			if( arr[middle - 1 ] < arr[middle] && arr[middle] > arr[middle + 1 ] ) {
-				return middle;
-			}
-
-			if( arr[middle - 1 ] < arr[middle] && arr[middle] < arr[middle + 1 ] ) {
-				//B     M     E
-				//1 2 3 4 5 6 1
-				begin = middle + 1;
-			}
-
-			if( arr[middle - 1 ] > arr[middle] && arr[middle] > arr[middle + 1 ] ) {
-				//B     M     E
-				//1 6 5 4 3 2 1
-				end = middle - 1;
-			}
-		}
-
-		return -1;
+        int end = arr.length - 1;
+        
+        while( begin <= end ) {
+            
+            int middle = ( begin + end ) / 2;
+            
+            if( arr[middle] < arr[middle + 1 ] ) {
+                begin = middle + 1;
+            } else {
+                end = middle;
+            }
+        }
+        
+        return begin;
 	}
 }
