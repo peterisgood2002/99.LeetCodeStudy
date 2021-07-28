4ed883dc58426d509aa4649a2b4336a7b684126f
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-07-28 08:46:24 +0800

    [MEDIUM_0436_FIND_RIGHT_INTERVAL] Solution

--- src/main/java/com/peter12/solution/medium/MEDIUM_0436_FIND_RIGHT_INTERVAL.java
+++ src/main/java/com/peter12/solution/medium/MEDIUM_0436_FIND_RIGHT_INTERVAL.java
@@ -3,80 +3,66 @@ package com.peter12.solution.medium;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class MEDIUM_0436_FIND_RIGHT_INTERVAL {
-	public class Interval implements Comparable<Interval> {
-        public int index;
-        public int[] intervals;
-        
-        public Interval( int i, int[] a ) {
-            index = i;
-            intervals = a;
-        }
-        
-        public int compareTo( Interval o ) {
-            return  intervals[0] - o.intervals[0];
-                
-        }
-        
-        public String toString() {
-            return "[" + intervals[0] + "," + intervals[1]+ "]";
-        }
-        
-    }
-    public int[] findRightInterval(int[][] intervals) {
-        
-        List<Interval> data = new ArrayList<Interval>();
-        for( int i = 0; i < intervals.length; i++ ) {
-            data.add( new Interval( i, intervals[i] ) );
-        }
-        
-        Collections.sort( data );
-        
-        for( int i = 0; i < data.size(); i++ ) {
-            System.out.println( data.get(i) );
-        }
-        
-        int[] result = new int[data.size()];
-        
-        for( int i = 0; i < data.size(); i++ ) {
-            
-            Interval left = data.get(i);
-            result[left.index] = rightIndex(left, data);
-            
-        }
-        
-        
-        return result;
-    }
-    
-    public int rightIndex( Interval t, List<Interval> data) {
-        int begin = 0;
-        int end = data.size() - 1;
-        int target = t.intervals[1];
-        while( begin <= end ) {
-            
-            int middle = ( begin + end ) / 2;
-            
-            Interval d = data.get( middle );
-            
-            if(target == d.intervals[0] ) {
-                return d.index;
-            }
-            
-            if( target < d.intervals[0] ) {
-                end = middle - 1;
-            } else {
-                begin = middle + 1;
-            }
-        }
-        
-        if( begin < data.size() ) {
-            return data.get(begin).index;
-        } else {
-            return -1;
-        }
-        
-    }
+	public int[] findRightInterval(int[][] intervals) {
+
+		/*
+	          Solution: https://www.cnblogs.com/grandyang/p/6018581.html
+		 */
+		HashMap<Integer, Integer> index = new HashMap<Integer, Integer>();
+		int[] start = new int[ intervals.length];
+		for( int i = 0; i < intervals.length; i++ ) {
+			start[i] =  intervals[i][0];
+
+			index.put( intervals[i][0], i);
+		}
+
+		Arrays.sort( start );
+		int[] result = new int[ intervals.length];
+		for( int i = 0; i < intervals.length; i++ ) {
+			int rv = findValue( intervals[i][1], start);
+
+			Integer ri = index.get(rv);
+
+			if( ri != null ) {
+				result[i] = ri;
+			} else {
+				result[i] = -1;
+			}
+
+
+		}
+
+		return result;
+
+	}
+
+	public int findValue( int target, int[] data ) {
+		int begin = 0;
+		int end = data.length - 1;
+
+		while( begin <= end ) {
+			int middle = ( begin + end ) / 2;
+
+			if( data[middle] == target ) {
+				return data[middle];
+			}
+
+			if( target < data[middle] ) {
+				end = middle - 1;
+			} else {
+				begin = middle + 1;
+			}
+		} 
+
+		if( begin < data.length ) {
+			return data[begin];
+		} else {
+			return Integer.MIN_VALUE;
+		}
+
+	}
 }
