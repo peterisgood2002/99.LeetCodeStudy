66172bef8873648c7638585e874062bdd4dd7fc7
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-07-29 10:33:42 +0800

    [EASY_0709_TO_LOWER_CASE] complete

---
+++ src/main/java/com/peter12/solution/easy/EASY_0709_TO_LOWER_CASE.java
@@ -0,0 +1,13 @@
+package com.peter12.solution.easy;
+
+public class EASY_0709_TO_LOWER_CASE {
+	public String toLowerCase(String s) {
+        char[] ch = s.toCharArray();
+        
+        for(int i = 0; i < ch.length; i++ ) {
+        	ch[i] = (char) (ch[i] - 'A' + 'a' );
+        }
+        
+        return new String(ch);
+    }
+}
