bdc4d2d95be082480b49f797a78f462a2312ec76
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-07-29 11:31:45 +0800

    [EASY_0771_JEWELS_AND_STONES] complete

---
+++ src/main/java/com/peter12/solution/easy/EASY_0771_JEWELS_AND_STONES.java
@@ -0,0 +1,27 @@
+package com.peter12.solution.easy;
+
+public class EASY_0771_JEWELS_AND_STONES {
+	public int numJewelsInStones(String jewels, String stones) {
+
+		HashMap<Character,Integer> count = new HashMap<Character,Integer>();
+		for( char ch : jewels.toCharArray() ) {
+			Integer c = count.get(ch);
+			if( c == null ) {
+				c = 0;
+			}
+
+			c++;
+			count.put( ch, c);
+		}
+
+		int result = 0;
+		for( char ch : stones.toCharArray() ) {
+			Integer c = count.get(ch);
+			if( c != null ) {
+				result++;
+			}
+		}
+
+		return result;
+	}
+}
