6a9aba289ea1e92a18b37f39b5bac5caadeecca6
Zhi-Cheng Yan <peterisgood2002@gmail.com>
2021-08-23 22:44:56 +0800

    [EASY_0144_BINARY_TREE_PREORDER_TRAVERSAL] complete

--- src/main/java/com/peter12/solution/easy/EASY_0144_BINARY_TREE_PREORDER_TRAVERSAL.java
+++ src/main/java/com/peter12/solution/easy/EASY_0144_BINARY_TREE_PREORDER_TRAVERSAL.java
@@ -1,5 +1,28 @@
 package com.peter12.solution.easy;
 
-public class EASY_0144_BINARY_TREE_PREORDER_TRAVERSAL {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.peter12.solution.data.TreeNode;
+
+public class EASY_0144_BINARY_TREE_PREORDER_TRAVERSAL {
+	public List<Integer> preorderTraversal(TreeNode root) {
+
+		List<Integer> result = new ArrayList<Integer>();
+
+		preorder(root, result);
+		return result;
+	}
+
+	public void preorder( TreeNode root, List<Integer> result ) {
+
+		if( root == null ) {
+			return;
+		}
+
+		result.add( root.val);
+
+		preorder( root.left, result);
+		preorder( root.right, result);
+	}
 }
